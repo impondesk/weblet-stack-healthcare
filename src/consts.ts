45f0d99bdd71e116dec702739308e109bffa9730
Israni, Murli <murli.p.israni@gmail.com>
2025-12-19 23:38:46 +0530

    fix: Update site title to "Weblet Platforms" for consistency across the application

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Narr8 App";
+export const SITE_TITLE = "Weblet Platforms";
 export const SITE_DESCRIPTION = "Welcome to story stack!";
 
 export const defaultTheme = {
