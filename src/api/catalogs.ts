bce7c2a987d2e2c8bb0148b8353a7d1a4541ef7e
Israni, Murli <murli.p.israni@gmail.com>
2025-12-30 18:33:14 +0530

    feat: Implement catalog fetching and layout with fallback handling

---
+++ src/api/catalogs.ts
@@ -0,0 +1,27 @@
+import { apiFetch } from "./api-fetch";
+
+export async function getCatalogByDomain(_domain, slug) {
+  const url = `${
+    import.meta.env.PUBLIC_API_URL
+  }/catalogs?where[tenant.domains.domain][equals]=${_domain}&where[_status][equals]=published&where[slug][equals]=${slug}&depth=2`;
+  const response = await apiFetch(url, {});
+  return response;
+}
+
+export async function getCatalogsByDomain(_domain) {
+  const url = `${
+    import.meta.env.PUBLIC_API_URL
+  }/catalogs?where[tenant.domains.domain][equals]=${_domain}&where[_status][equals]=published&where[visibility][equals]=public&depth=2`;
+
+  const response = await apiFetch(url, {});
+
+  return response;
+}
+
+// export async function getDataByCatalog(_domain, slug) {
+//   const url = `${
+//     import.meta.env.PUBLIC_API_URL
+//   }/catalogs/getDataByCatalog?domain=${_domain}&slug=${slug}`;
+//   const response = await apiFetch(url, {});
+//   return response;
+// }
